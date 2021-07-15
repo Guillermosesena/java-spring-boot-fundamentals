c7bd26a4f61cf710cb47b304a9f38d4c97a2eedd
Guillermo Sesena <guillermosesena@gmail.com>
2021-07-14 19:08:15 -0500

    18/31 Configuring datasource

--- src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -6,9 +6,12 @@ import com.fundamentos.springboot.fundamentos.bean.MyBeanWithPropertiesImplement
 import com.fundamentos.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.sql.DataSource;
 
 @Configuration
 //UserPojo es la clase que se va a representar
@@ -26,5 +29,15 @@ public class GeneralConfiguration {
     @Bean
     public MyBeanWithProperties fuction(){
         return new MyBeanWithPropertiesImplement(name, apellido);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
     }
 }
