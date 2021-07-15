7e1dd021507d33b66363cd4c25c46422d759797b
Guillermo Sesena <guillermosesena@gmail.com>
2021-07-14 22:15:02 -0500

    21/31 Using value annotation for properties

--- src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -9,11 +9,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:connection.properties")
 //UserPojo es la clase que se va a representar
 @EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
@@ -26,6 +28,20 @@ public class GeneralConfiguration {
     @Value("${value.random}")
     private String random;
 
+    @Value("${jdbc.url}")
+    private String jdbcurl;
+
+    @Value("${driver}")
+    private String driver;
+
+    @Value("${username}")
+    private String username;
+
+    @Value("${password}")
+    private String password;
+
+
+
     @Bean
     public MyBeanWithProperties fuction(){
         return new MyBeanWithPropertiesImplement(name, apellido);
@@ -34,10 +50,10 @@ public class GeneralConfiguration {
     @Bean
     public DataSource dataSource(){
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:testdb");
-        dataSourceBuilder.username("SA");
-        dataSourceBuilder.password("");
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(jdbcurl);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
         return dataSourceBuilder.build();
     }
 }
