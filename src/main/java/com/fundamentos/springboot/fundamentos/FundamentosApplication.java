166d682eaac596c0962f554ca29c798ba45478a2
Guillermo Sesena <guillermosesena@gmail.com>
2021-07-14 21:57:52 -0500

    18/31 Configuring datasource

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.domain.Sort;
 
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -56,7 +57,17 @@ public class FundamentosApplication implements CommandLineRunner {
 	public void run(String... args) {
 		//ejemplosAnteriores();
 		saveUserInDataBase();
+		getInformationJpqlFromUser();
+	}
 
+	private void getInformationJpqlFromUser(){
+		LOGGER.info("Usuario con el método finByUserEmail" +
+				userRepository.finByUserEmail("julie@domain.com")
+				.orElseThrow(()->new RuntimeException("No se encontró el usuario")));
+
+		userRepository.findAndSort("user", Sort.by("id").ascending())
+					.stream()
+					.forEach(user -> LOGGER.info("Usuario con metodo sort" + user));
 	}
 
 	private void saveUserInDataBase(){
